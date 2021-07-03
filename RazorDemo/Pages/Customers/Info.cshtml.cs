30b664d0293abd9045008574a7040973926a1328
Kaushalya Harshanath <wakharshanath@gmail.com>
2021-07-03 08:07:23 +0530

    add not found page and back to home button/link

--- RazorDemo/Pages/Customers/Info.cshtml.cs
+++ RazorDemo/Pages/Customers/Info.cshtml.cs
@@ -1,28 +1,32 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Razor.Models;
-using Razor.Services;
-
 namespace Razor.Demo.Pages.Customers
 {
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Razor.Models;
+    using Razor.Services;
+
     public class InfoModel : PageModel
     {
         private readonly ICustomerRepository _customerRepository;
-        public Customer customer { get; private set;}
+        public Customer customer { get; private set; }
         public InfoModel(ICustomerRepository customerRepository)
         {
             _customerRepository = customerRepository;
         }
-        [BindProperty(SupportsGet =true)]
+        [BindProperty(SupportsGet = true)]
         public int id { get; set; }
-        public void OnGet(int Id)
+        public IActionResult OnGet(int Id)
         {
-           customer =
-                _customerRepository.GetCustomer(Id);
+            customer =
+                 _customerRepository.GetCustomer(Id);
+            if (id.Equals(0))
+            {
+                return RedirectToPage("/NotFound");
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }
